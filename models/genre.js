7154a87dad74c88b0f6d280b5a67bf7534e58927
Farid Ali <faridalicode@gmail.com>
2021-04-17 11:51:06 +0530

    renamed genere to genre

--- models/genre.js
+++ models/genre.js
@@ -2,18 +2,18 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 //setup genere schema
-var GenereSchema = new Schema(
+var GenreSchema = new Schema(
   {
     name: {type: String, required: true, minLength: 3, maxLength: 100},
   }
 );
 
 //virtual for genere's URL
-GenereSchema
+GenreSchema
   .virtual('url')
   .get(function() {
     return '/catalog/genere/' + this._id;
   });
 
   //export model
-  module.exports = mongoose.model('Genere', GenereSchema);
+  module.exports = mongoose.model('Genere', GenreSchema);
