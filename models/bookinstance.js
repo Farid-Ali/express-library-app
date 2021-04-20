2fdfd04e899ceebcc9b91c29361e2c9199d8fe28
Farid Ali <faridalicode@gmail.com>
2021-04-20 19:42:22 +0530

    formated due_date in bookinstances list page

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 //setup bookinstance schema
 var BookInstanceSchema = new Schema(
@@ -17,6 +18,12 @@ BookInstanceSchema
   .get(function() {
     return '/catalog/bookinstance/' + this._id;
   });
+
+  BookInstanceSchema
+    .virtual('due_back_formatted')
+    .get(function() {
+      return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+    });
 
   //export model
   module.exports = mongoose.model('BookInstance', BookInstanceSchema);
