1344fa0f5b2f52d5bf9961c335953a672ddd95a1
Farid Ali <faridalicode@gmail.com>
2021-04-23 20:05:40 +0530

    added author detail page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,6 @@
 var Author = require('../models/author');
+var async = require('async');
+var Book = require('../models/book');
 
 //Display list of all authors
 exports.author_list = function(req, res) {
@@ -13,7 +15,25 @@ exports.author_list = function(req, res) {
 
 //Display detail page for a specific author
 exports.author_detail = function(req, res) {
-  res.send('Not Implemented: Author detail: ' + req.params.id);
+  
+  async.parallel({
+    author: function(callback) {
+      Author.findById(req.params.id)
+        .exec(callback)
+    },
+    authors_books: function(callback) {
+      Book.find({ 'author': req.params.id }, 'title summary')
+        .exec(callback)
+    },
+  }, function(err, results) {
+    if (err) { return next(err); }
+    if (results.author == null ) {
+      var err = new Error('Author not found');
+      err.status = 404;
+      return next(err);
+    }
+    res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books });
+  });
 };
 
 //Display author create form on GET
